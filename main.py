7db767aed8d9d1ce0ead63f2614cf158188b42c1
Konstantinos <sofronas.konstantinos@gmail.com>
2023-02-07 10:21:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from datetime import datetime
 from plyer import notification
 
 def main():
+    first_time_running = True
     tme = datetime.now().strftime("%H:%M:%S %d-%m-%y")
     while True:
-        print("Stand up: Notification - {}".format(tme))
-        notification.notify(
-            #title of the notification,
-            title = "Stand up: {}".format(tme),
-            #the body of the notification
-            message = "Half hour has passed",  
-            #creating icon for the notification
-            #we need to download a icon of ico file format
-            app_icon = "chair-stand.ico",
-            # the notification stays for 50sec
-            timeout  = 50
-        )
+        if first_time_running == True:
+            print("First time: {}".format(tme))
+            first_time_running = False
+        else:
+            print("Stand up: Notification - {}".format(tme))
+            notification.notify(
+                #title of the notification,
+                title = "Stand up: {}".format(tme),
+                #the body of the notification
+                message = "Half hour has passed",  
+                #creating icon for the notification
+                #we need to download a icon of ico file format
+                app_icon = "chair-stand.ico",
+                # the notification stays for 50sec
+                timeout  = 50
+            )
         time.sleep(1800)
 
 if __name__ == "__main__":
